6a262d526aa50897736874a18bb1fe6ef38daadc
Akshay Palya <akshaypalya@Akshays-Air.domain.name>
2025-02-04 21:21:00 +0530

    testing

--- stock/tailwind.config.ts
+++ stock/tailwind.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
         sm: 'calc(var(--radius) - 4px)',
       },
       colors: {
-        background: 'hsl(var(--background))',
+        background: 'hsl(var(--background))',//testing 
         foreground: 'hsl(var(--foreground))',
         card: {
           DEFAULT: 'hsl(var(--card))',
